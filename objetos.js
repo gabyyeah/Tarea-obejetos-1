127285e871f4a4e7a7f030f92b13f4c83c9642ed
gabyyeah <gabymoreno181972@gmail.com>
2021-01-23 11:29:55 -0500

    Commit Inicial

---
+++ objetos.js
@@ -0,0 +1,41 @@
+let vehículo1= {
+    marca: "Chevrolet",
+    modelo: "Aveo",
+    year: 2002,
+    kilometraje: "5km",
+    kpg: "32 kilómetros por galón"
+}
+
+let vehículo2= {
+    marca: "BMW",
+    modelo: "X5",
+    year: 1999,
+    kilometraje: "7km",
+    kpg: "53.2 kilómetros por galón"
+}
+
+let vehículo3= {
+    marca: "Audi",
+    modelo: "A7",
+    year: 2011,
+    kilometraje: "8km",
+    kpg: "50.007 kilómetros por galón"
+}
+
+let vehículo4= {
+    marca: "Fiat",
+    modelo: "Freemont",
+    year: 2016,
+    kilometraje: "10km",
+    kpg: "43 kilómetros por galón"
+}
+
+
+let vehículos=[vehículo1, vehículo2, vehículo3, vehículo4]
+
+let conductor={
+    nombre: "Ricky",
+    edad: "19 años",
+    ciudad: "Madrid",
+    vehículos: vehículos
+}
